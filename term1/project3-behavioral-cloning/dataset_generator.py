d0092ca909452e060fd21acb6b9b9bf306d30340
yasenh <yasenhu789@gmail.com>
2016-12-09 23:15:33 -0500

    add README

--- term1/project3-behavioral-cloning/dataset_generator.py
+++ term1/project3-behavioral-cloning/dataset_generator.py
@@ -4,12 +4,13 @@ import numpy as np
 import cv2
 import os
 
-
+# input image info - height, width and channel, resize image to avoid out of memory issue
 img_h = 64
 img_w = 128
 n_channel = 3
 
 with open('./driving_log.csv', 'rt') as f:
+    # get exact number of lines in log, so we can define the array size at the beginning
     n = len(f.readlines()) * 3
     print(n)
 
@@ -19,6 +20,7 @@ with open('./driving_log.csv', 'rt') as f:
     reader = csv.reader(f)
     count = 0
     for line in reader:
+        # use center images, and left/right images with offset steering angle as training data
         img_center = cv2.imread(line[0])
         img_center = cv2.resize(img_center,(img_w, img_h))
         imgs[count] = img_center
@@ -41,7 +43,6 @@ with open('./driving_log.csv', 'rt') as f:
         labels[count] = label_right
         count += 1
 
-
         #print log
         if (count % 30 == 0):
             print(count)
@@ -53,6 +54,7 @@ training_file = 'train.p'
 if os.path.isfile(training_file):
     os.remove(training_file)
 
+# output to train.p
 with open(training_file, 'wb') as handle:
     dict = {'features': imgs, 'angles': labels}
     pickle.dump(dict, handle, protocol=4)
--- term1/project3-behavioral-cloning/model.py
+++ term1/project3-behavioral-cloning/model.py
@@ -5,14 +5,18 @@ import pickle
 import json
 import os
 
+# input image info - height, width and channel
 img_h = 64
 img_w = 128
 n_channel = 3
-
+input_shape = (img_h, img_w, n_channel)
 # size of pooling area for max pooling
 pool_size = (2, 2)
-input_shape = (img_h, img_w, 3)
 
+# both Sequential model and Function API are used for practice
+# model is based on the NVIDIA's paper - "End to End Learning for Self-Driving Cars"
+# https://arxiv.org/pdf/1604.07316v1.pdf
+# add convolution layers to abstract feature map
 base_model = Sequential()
 base_model.add(Convolution2D(24, 5, 5,
                         activation = 'relu',
@@ -24,30 +28,36 @@ base_model.add(MaxPooling2D(pool_size = pool_size))
 base_model.add(Convolution2D(96, 3, 3,
                         activation = 'relu'))
 base_model.add(MaxPooling2D(pool_size = pool_size))
+# add dropout layer to avoid overfitting
 base_model.add(Dropout(0.25))
-
+# The base model can be placed by other pre-trained models such as VGG16
+# use function API
 x = base_model.output
-#Add the fully-connected layers
+# add the fully-connected layers
 x = Flatten(name='flatten')(x)
 x = Dense(1164, activation='relu', name='fc1')(x)
 x = Dense(100, activation='relu', name='fc2')(x)
 x = Dense(50, activation='relu', name='fc3')(x)
 x = Dense(10, activation='relu', name='fc4')(x)
+# It is a regression problem, so only 1 output
 predictions = Dense(1, name='predictions')(x)
-
 model = Model(input = base_model.input, output = predictions)
+# print model architecture
 model.summary()
-
+# use "Adam" optimizer and mean square error for regression problem
 model.compile(optimizer = 'adam', loss = 'mse')
 
+# load training data
 training_file = './train.p'
 with open(training_file, mode='rb') as f:
     train = pickle.load(f)
 
 X_train, Y_train = train['features'], train['angles']
+# normalization to [-0.5, 0.5]
 X_train /= 255
 X_train -= 0.5
 
+# split dataset to training, validation and testing set
 train_features, test_features, train_labels, test_labels = train_test_split(
     X_train,
     Y_train,
@@ -60,26 +70,28 @@ train_features, valid_features, train_labels, valid_labels = train_test_split(
     test_size=0.20,
     random_state=832289)
 
+# define output files
 model_json_file = 'model.json'
 model_h5_file = 'model.h5'
 
+# remove old results
+# TODO: check old model and weights, load them for finetuning
 if os.path.isfile(model_json_file):
     os.remove(model_json_file)
-
 if os.path.isfile(model_h5_file):
     os.remove(model_h5_file)
 
-nb_epoch = 3
+# set training parameters
+nb_epoch = 10
 batch_size = 32
 
-#fits the model on batches with real-time data augmentation:
+# training the model
 history = model.fit(train_features, train_labels,
                     batch_size = batch_size, nb_epoch = nb_epoch,
                     validation_data=(valid_features, valid_labels))
-
+# evaluation
 score = model.evaluate(test_features, test_labels)
-print('Test score:', score[0])
-print('Test accuracy:', score[1])
+print('Test score(loss):', score)
 
 # Save your model architecture as model.json, and the weights as model.h5.
 # serialize model to JSON

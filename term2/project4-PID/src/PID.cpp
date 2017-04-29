17877e9c234ff7206e019f84c89ca1750a4af851
yasenh <yasenhu789@gmail.com>
2017-04-28 22:28:48 -0400

    implement PID control

--- term2/project4-PID/src/PID.cpp
+++ term2/project4-PID/src/PID.cpp
@@ -6,16 +6,31 @@ using namespace std;
 * TODO: Complete the PID class.
 */
 
-PID::PID() {}
+PID::PID() {
+    p_error_ = i_error_ = d_error_ = 0;
+    prev_cte_ = 0;
+}
 
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    Kp_ = Kp;
+    Ki_ = Ki;
+    Kd_ = Kd;
 }
 
 void PID::UpdateError(double cte) {
+    p_error_ = cte;
+
+    d_error_ = cte - prev_cte_;
+    prev_cte_ = cte;
+
+    i_error_ += cte;
+
 }
 
 double PID::TotalError() {
+    double total_error = Kp_ * p_error_ + Ki_ * i_error_ + Kd_ * d_error_;
+    return total_error;
 }
 
--- term2/project4-PID/src/PID.h
+++ term2/project4-PID/src/PID.h
@@ -3,44 +3,51 @@
 
 class PID {
 public:
-  /*
-  * Errors
-  */
-  double p_error;
-  double i_error;
-  double d_error;
 
-  /*
-  * Coefficients
-  */ 
-  double Kp;
-  double Ki;
-  double Kd;
+    /*
+     * Constructor
+     */
+    PID();
 
-  /*
-  * Constructor
-  */
-  PID();
+    /*
+     * Destructor.
+     */
+    virtual ~PID();
 
-  /*
-  * Destructor.
-  */
-  virtual ~PID();
+    /*
+     * Initialize PID.
+     */
+    void Init(double Kp, double Ki, double Kd);
 
-  /*
-  * Initialize PID.
-  */
-  void Init(double Kp, double Ki, double Kd);
+    /*
+     * Update the PID error variables given cross track error.
+     */
+    void UpdateError(double cte);
 
-  /*
-  * Update the PID error variables given cross track error.
-  */
-  void UpdateError(double cte);
+    /*
+     * Calculate the total PID error.
+     */
+    double TotalError();
 
-  /*
-  * Calculate the total PID error.
-  */
-  double TotalError();
+private:
+
+    /*
+     * Errors
+     */
+    double p_error_;
+    double i_error_;
+    double d_error_;
+
+    /*
+     * Coefficients
+     */
+    double Kp_;
+    double Ki_;
+    double Kd_;
+
+    double prev_cte_;
+
+
 };
 
 #endif /* PID_H */

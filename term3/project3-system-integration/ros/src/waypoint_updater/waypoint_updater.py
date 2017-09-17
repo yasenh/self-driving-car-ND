0f43c22e04230041929ed7e8f21f94e771b22763
yasenh <yasenhu789@gmail.com>
2017-09-17 13:48:31 -0400

    add waypoints around vehicle

--- term3/project3-system-integration/ros/src/waypoint_updater/waypoint_updater.py
+++ term3/project3-system-integration/ros/src/waypoint_updater/waypoint_updater.py
@@ -22,8 +22,8 @@ as well as to verify your TL classifier.
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
-
+LOOK_AHEAD_WPS = 5 # Number of waypoints we will publish. You can change this number
+LOOK_BEHIND_WPS = 5
 
 class WaypointUpdater(object):
     def __init__(self):
@@ -54,6 +54,14 @@ class WaypointUpdater(object):
 
     def waypoints_cb(self, msg):
         self.waypoints = msg.waypoints
+        waypoints_num = len(self.waypoints)
+        waypoint_around_host_vehicle = self.get_waypoint_around_host_vehicle(self.waypoints, waypoints_num)
+
+        lane = Lane()
+        lane.header.stamp = rospy.Time.now()
+        lane.waypoints = waypoint_around_host_vehicle
+        self.final_waypoints_pub.publish(lane)
+
         pass
 
     def traffic_cb(self, msg):
@@ -78,6 +86,67 @@ class WaypointUpdater(object):
             wp1 = i
         return dist
 
+    def get_waypoint_around_host_vehicle(self, waypoints, waypoints_num):
+        car_position_index = self.get_closest_waypoint(self.pose)
+        print "host =" + str(car_position_index)
+
+        behind_waypoint = car_position_index - LOOK_BEHIND_WPS
+        ahead_waypoint = car_position_index + LOOK_AHEAD_WPS
+
+        # print behind_waypoint
+        # print ahead_waypoint
+
+        if behind_waypoint < 0:
+            behind_waypoint += waypoints_num
+            ahead_waypoint += waypoints_num
+
+        if behind_waypoint < 0 or ahead_waypoint > waypoints_num:
+            waypoints = waypoints + waypoints
+
+        # waypoint_around_host_vehicle = waypoints[behind_waypoint: ahead_waypoint]
+
+        waypoint_around_host_vehicle = waypoints[car_position_index + 1: ahead_waypoint]
+
+        # for wp in waypoint_around_host_vehicle:
+        #     print self.get_closest_waypoint(wp.pose.pose)
+
+        # TODO: smooth path with evenly distance
+
+        return waypoint_around_host_vehicle
+
+    def get_closest_waypoint(self, pose):
+        """Identifies the closest path waypoint to the given position
+            https://en.wikipedia.org/wiki/Closest_pair_of_points_problem
+        Args:
+            pose (Pose): position to match a waypoint to
+
+        Returns:
+            int: index of the closest waypoint in self.waypoints
+
+        """
+        # TODO: Think about orientation in front
+
+        search_distance = 5.0
+        vehicle_x = pose.position.x
+        vehicle_y = pose.position.y
+
+        min_distance = 9999
+        min_index = None
+
+        for i, waypoint in enumerate(self.waypoints):
+            wp_x = waypoint.pose.pose.position.x
+            wp_y = waypoint.pose.pose.position.y
+            dx = wp_x - vehicle_x
+            dy = wp_y - vehicle_y
+            distance = (dx ** 2 + dy ** 2) ** 0.5
+            if distance > search_distance:
+                continue
+
+            if distance < min_distance:
+                min_distance = distance
+                min_index = i
+
+        return min_index
 
 if __name__ == '__main__':
     try:
